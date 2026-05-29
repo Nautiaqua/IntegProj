86e1d69482226606b6c2b544ca7811e78d06c943
Nautiaqua <sailor.candie@gmail.com>
2026-05-29 14:04:04 +0800

    added login screen ui

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>CloudTravels Resume Portal</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=mail" />
+
+        <!-- bootstrap components -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    </head>
+    <body id="mainbody" data-bs-theme="light">
+        <div class="container-fluid d-flex align-items-center min-vh-100">
+            <div id="mainlogin" class="container p-5 bg-body-primary" style="border-radius: 1.5rem; width: 25rem;">
+                <div class="row d-flex align-items-center justify-content-center">
+                    <img id="loginlogo" src="Assets/roundlogo.png" class="img-fluid" style="width: 5rem;"><br><br><br>
+                </div>
+                <div class="row"><div class="col d-flex justify-content-center" style="font-family: 'Montserrat'; font-size: 1.3rem;"><b>Resume Portal</b></div></div>
+                <div class="row d-flex justify-content-center">Please enter your e-mail.</div>
+
+                <div class="row d-flex justify-content-center"><hr class="my-3" style="width: 20rem;"></tediv>
+                <div class="row">
+                    <div class="col">
+                        <form class="d-flex flex-column justify-content-center gap-3">
+                            <input type="text" name="txtEmail" style="width: 200px; border: none; border-radius: 0.6rem; width: stretch;" class="bg-body-secondary p-2" placeholder="&nbsp;Email" required/>
+                            <button type="button" class="btn btn-primary" style="border-radius: 0.6rem; background-color: #0b81db;">Proceed</button>
+                        </form>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- bootstrap json -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </body>
+</html>
