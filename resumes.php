dacb72070586098265bf59e9d3fdc9a64fa4de9a
Marc Iris <sepnio.marciris@ue.edu.ph>
2026-05-30 12:20:27 +0800

    updating resumes.php add folder resumes with json files

--- resumes.php
+++ resumes.php
@@ -17,10 +17,67 @@
     </head>
     <body data-bs-theme="light">
         <div class="container-fluid">
-            <!-- content in here -->
+            <h2>Resume Portal</h2>
+            Search Applicant:
+            <input type="text" id="searchInput">
+
+            <br><br>
+
+            <table border="1" cellpadding="5" id="applicantTable">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Position</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+
+                    $files = glob("resumes/*.json");
+
+                    foreach($files as $file){
+
+                        $json = file_get_contents($file);
+                        $data = json_decode($json,true);
+
+                        echo "<tr>";
+                        echo "<td>".$data['name']."</td>";
+                        echo "<td>".$data['email']."</td>";
+                        echo "<td>".$data['applyingfor']."</td>";
+                        echo "</tr>";
+                    }
+
+                    ?>
+                </tbody>
+            </table>
         </div>
 
         <!-- bootstrap json -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </body>
+    <script>
+
+document.getElementById("searchInput")
+.addEventListener("keyup", function(){
+
+    var value = this.value.toLowerCase();
+
+    var rows =
+    document.querySelectorAll("#applicantTable tbody tr");
+
+    rows.forEach(function(row){
+
+        if(row.textContent.toLowerCase().includes(value)){
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+
+    });
+
+});
+
+</script>
 </html>
